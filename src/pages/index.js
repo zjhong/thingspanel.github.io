15d63d3fd1d6d5b979110ccbf28a94cde8678a0e
zjhong <zjhong@gmail.com>
2024-09-04 07:21:37 +0800

    fix gogole adsense

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import AdSense from '@site/src/components/AdSense';
 
 function Home() {
   return (
     <Layout>
       <main>
-        {/* 您的页面内容 */}
-        <AdSense client="ca-pub-3936883179210897" slot="9166877410" />
-        {/* 更多页面内容 */}
+        {/* 您的首页内容 */}
       </main>
     </Layout>
   );
