3f26ccbfd6f2dc3e0ae4b6093807b9691beec960
hezhuozhuo <421045249@qq.com>
2024-09-04 18:22:05 +0800

    fix

--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import DocItemLayout from '@theme-original/DocItem/Layout';
-import AdSense from '@site/src/components/AdSense';
+// import AdSense from '@site/src/components/AdSense';
 
 export default function DocItemLayoutWrapper(props) {
   return (
     <>
       <DocItemLayout {...props} />
-      <AdSense client="ca-pub-3936883179210897" slot="9166877410" />
+      {/* <AdSense client="ca-pub-3936883179210897" slot="9166877410" /> */}
     </>
   );
 }
